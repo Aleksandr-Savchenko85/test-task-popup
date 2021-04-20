7612a5221009274117975b98b0ed2b015102c815
tests2 <https://github.com/roman1501/tests2.git>
2021-04-20 12:31:40 +0300

    commit

--- src/ButtonClosePopup.jsx
+++ src/ButtonClosePopup.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ButtonClosePopup = ({ hideForm }) => {
     return (
         <>
-            <div className="trigger"><span className="text-trigger">100%jnnjn</span></div>
+            <div className="trigger"><span className="text-trigger">100%</span></div>
             <button className="close_btn" onClick={hideForm}><span className="text-close_btn">X</span></button>
         </>
     )
